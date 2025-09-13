4a7c061f6aaec42992efd45c61c2f27ec86e87a9
Ernesto Missael Contreras Emigdio <98929241+Erst086@users.noreply.github.com>
2025-09-13 02:48:08 -0600

    run.py

--- back_end/run.py
+++ back_end/run.py
@@ -2,7 +2,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)  # Esto debe ir aquí, antes de app.run()
+CORS(app)  # Hola BUenas tarde o noches, esto debe de ir aqui :3
 
 if __name__ == "__main__":
     app.run(debug=True)
